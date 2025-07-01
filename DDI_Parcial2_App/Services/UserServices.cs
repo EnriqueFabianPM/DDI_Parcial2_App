dba96e43d5827d1ae36c8d6705177a4832ae0cb5
EnriqueFabianPM <tuemail@example.com>
2025-07-01 05:51:51 -0600

    Commit_2: Eliminacion de barra lateral y de navegacion

--- DDI_Parcial2_App/Services/UserServices.cs
+++ DDI_Parcial2_App/Services/UserServices.cs
@@ -16,7 +16,7 @@ namespace DDI_Parcial2_App.Services
             new User { Nombre = "Daniel FLores", Email = "21008@virtual.utsc.edu.mx", Password = "21008" },
             new User { Nombre = "Elias Rafael", Email = "21017@virtual.utsc.edu.mx", Password = "21017" },
             new User { Nombre = "Edgardo Puente", Email = "21026@virtual.utsc.edu.mx", Password = "21026" },
-            new User { Nombre = "Joel Alexander", Email = "21024@virtual.utsc.edu.mx", Password = "21160" },
+            new User { Nombre = "Joel Alexander", Email = "21160@virtual.utsc.edu.mx", Password = "21160" },
             new User { Nombre = "Kevin Aldahir", Email = "21136@virtual.utsc.edu.mx", Password = "21136" },
             new User { Nombre = "Ethan Andres", Email = "16246@virtual.utsc.edu.mx", Password = "16246" },
             new User { Nombre = "Paul Eduardo", Email = "18167@virtual.utsc.edu.mx", Password = "18167" }
